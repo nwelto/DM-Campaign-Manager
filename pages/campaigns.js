62a1bd55a4b930402ffc0131cbda949ca2a9c5cc
Nathan <nathopp@gmail.com>
2023-12-13 21:34:37 -0600

    added buttons to the character and campaign pages to add new ones.

--- pages/campaigns.js
+++ pages/campaigns.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
 import { useAuth } from '../utils/context/authContext';
 import { getCampaign } from '../api/campaignData';
 import CampaignCard from '../components/CampaignCard';
 
 function ShowCampaigns() {
   const [campaigns, setCampaigns] = useState([]);
-
   const { user } = useAuth();
+  const router = useRouter();
 
   const getAllCampaigns = () => {
     getCampaign(user.uid).then(setCampaigns);
@@ -16,8 +18,13 @@ function ShowCampaigns() {
     getAllCampaigns();
   }, []);
 
+  const handleAddCampaign = () => {
+    router.push('/campaign/new');
+  };
+
   return (
     <div className="text-center my-4">
+      <Button onClick={handleAddCampaign} variant="dark" className="signin-btn">Add A Campaign</Button>
       <div className="d-flex flex-wrap">
         {campaigns.map((campaign) => (
           <CampaignCard key={campaign.firebaseKey} campaignObj={campaign} onUpdate={getAllCampaigns} />
